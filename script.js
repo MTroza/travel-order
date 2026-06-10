1d40db0875deef13d6322306358468efa914701f
MTroza <michaeltroza@gmail.com>
2026-06-10 12:22:48 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,51 @@
+function generateTravelOrder(){
+
+document.getElementById("oNo").innerText =
+document.getElementById("toNo").value;
+
+document.getElementById("oDate").innerText =
+document.getElementById("date").value;
+
+document.getElementById("oName").innerText =
+document.getElementById("name").value;
+
+document.getElementById("oPosition").innerText =
+document.getElementById("position").value;
+
+document.getElementById("oSalary").innerText =
+document.getElementById("salary").value;
+
+document.getElementById("oStation").innerText =
+document.getElementById("station").value;
+
+document.getElementById("oDeparture").innerText =
+document.getElementById("departure").value;
+
+document.getElementById("oReturn").innerText =
+document.getElementById("return").value;
+
+document.getElementById("oDestination").innerText =
+document.getElementById("destination").value;
+
+document.getElementById("oPurpose").innerText =
+document.getElementById("purpose").value;
+
+document.getElementById("oObjective").innerText =
+document.getElementById("objective").value;
+
+document.getElementById("oPerdiem").innerText =
+document.getElementById("perdiem").value;
+
+document.getElementById("oLaborers").innerText =
+document.getElementById("laborers").value;
+
+document.getElementById("oAppropriation").innerText =
+document.getElementById("appropriation").value;
+
+document.getElementById("oTransport").innerText =
+document.getElementById("transport").value;
+
+document.getElementById("oRemarks").innerText =
+document.getElementById("remarks").value;
+
+}
