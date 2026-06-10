28ed186d06aba504573b90a549561a8e7312e70e
MTroza <michaeltroza@gmail.com>
2026-06-10 13:06:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,51 @@
-body{
-    font-family: Arial, sans-serif;
-    background:#f2f2f2;
-    margin:20px;
-}
+function generateTravelOrder(){
 
-.container{
-    max-width:1100px;
-    margin:auto;
-}
+document.getElementById("oNo").innerText =
+document.getElementById("toNo").value;
 
-.form-section{
-    background:white;
-    padding:20px;
-    border-radius:10px;
-}
+document.getElementById("oDate").innerText =
+document.getElementById("date").value;
 
-label{
-    display:block;
-    margin-top:10px;
-    font-weight:bold;
-}
+document.getElementById("oName").innerText =
+document.getElementById("name").value;
 
-input, textarea{
-    width:100%;
-    padding:8px;
-    margin-top:5px;
-}
+document.getElementById("oPosition").innerText =
+document.getElementById("position").value;
 
-button{
-    margin-top:20px;
-    padding:10px 20px;
-    cursor:pointer;
-}
+document.getElementById("oSalary").innerText =
+document.getElementById("salary").value;
 
-.travel-order{
-    background:white;
-    padding:40px;
-    margin-top:20px;
-    border:2px solid black;
-}
+document.getElementById("oStation").innerText =
+document.getElementById("station").value;
 
-.header{
-    text-align:center;
-}
+document.getElementById("oDeparture").innerText =
+document.getElementById("departure").value;
 
-.row{
-    display:flex;
-    justify-content:space-between;
-}
+document.getElementById("oReturn").innerText =
+document.getElementById("return").value;
 
-.signature{
-    display:flex;
-    justify-content:space-around;
-    margin-top:60px;
+document.getElementById("oDestination").innerText =
+document.getElementById("destination").value;
+
+document.getElementById("oPurpose").innerText =
+document.getElementById("purpose").value;
+
+document.getElementById("oObjective").innerText =
+document.getElementById("objective").value;
+
+document.getElementById("oPerdiem").innerText =
+document.getElementById("perdiem").value;
+
+document.getElementById("oLaborers").innerText =
+document.getElementById("laborers").value;
+
+document.getElementById("oAppropriation").innerText =
+document.getElementById("appropriation").value;
+
+document.getElementById("oTransport").innerText =
+document.getElementById("transport").value;
+
+document.getElementById("oRemarks").innerText =
+document.getElementById("remarks").value;
+
 }
